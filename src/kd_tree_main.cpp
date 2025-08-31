563aeb3421449a30332732d4591393af2e6600b0
Ben Frauenknecht <ben.frauenknecht@tum.de>
2025-08-31 15:23:13 +0200

    Fix main bug

--- src/kd_tree_main.cpp
+++ src/kd_tree_main.cpp
@@ -1,14 +1,15 @@
+#include <iostream>
 #include <vector>
 #include "KDTree/tree/KDTree.h"
 #include "KDTree/plane_selection/PlaneSelectionAlgorithm.h"
 
-namespace kdtree {
-    int main() {
-        std::vector<Array3> vertices{};
-        std::vector<IndexArray3> faces{};
-        PlaneSelectionAlgorithm::Algorithm algorithm{PlaneSelectionAlgorithm::Algorithm::LOG};
+int main() {
+    using namespace kdtree;
+    std::vector<Array3> vertices{};
+    std::vector<IndexArray3> faces{};
+    PlaneSelectionAlgorithm::Algorithm algorithm{PlaneSelectionAlgorithm::Algorithm::LOG};
 
-        KDTree tree{vertices, faces, algorithm};
-        return 0;
-    }
-} // namespace kdtree
+    KDTree tree{vertices, faces, algorithm};
+    std::cout << tree;
+    return 0;
+}
