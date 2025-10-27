c7cc3f430ad9f887ed104cbf3c02cb7bedf4c551
Ben Frauenknecht <ben.frauenknecht@tum.de>
2025-10-27 11:33:00 +0100

    Optimal Plane

--- src/KDTree/plane_selection/PlaneSelectionAlgorithm.h
+++ src/KDTree/plane_selection/PlaneSelectionAlgorithm.h
@@ -8,6 +8,7 @@
 #include <variant>
 
 #include "KDTree/tree/KdDefinitions.h"
+#include "KDTree/tree/SplitParam.h"
 
 namespace kdtree {
 
@@ -29,6 +30,60 @@ namespace kdtree {
             QUADRATIC,
             LOGSQUARED,
             LOG
+        };
+
+        template<template<size_t> typename ContainerVectors>
+        ContainerVectors<2> addEqualPointsToSubset(ContainerVectors<3> subsets, bool minSideChosen) {
+            const size_t index = minSideChosen ? 0 : 1;
+            subsets[index]->insert(subsets[index]->end(), subsets[2]->begin(), subsets[2]->end());
+            return {(std::move(subsets[0])), (std::move(subsets[1]))};
+        }
+
+        template<typename... CallbackArgs>
+        class OptimalPlane final {
+            std::mutex planeMutex{};
+
+            double cost = std::numeric_limits<double>::infinity();
+
+            Plane optPlane;
+
+            std::tuple<std::decay_t<CallbackArgs>...> callbackArgs;
+
+            std::function<std::variant<TriangleIndexVectors<2>, PlaneEventVectors<2>>(std::decay_t<CallbackArgs> &&...)> boundGeometrySplit;
+
+        public:
+            const SplitParam &splitParam;
+
+            virtual ~OptimalPlane() = default;
+
+            explicit OptimalPlane(const SplitParam &splitParam, const std::function<std::variant<TriangleIndexVectors<2>, PlaneEventVectors<2>>(CallbackArgs...)> &boundPointsSplit) : optPlane(0, splitParam.splitDirection), boundGeometrySplit(boundPointsSplit), splitParam(splitParam) {
+            }
+
+            void evaluatePlane(const Plane &candidatePlane, const double candidateCost, CallbackArgs... args) {
+                std::unique_lock<std::mutex> lock(planeMutex);
+                // this if-clause exists to consistently build the same KDTree (choose plane with lower coordinate) by eliminating indeterministic behavior should the cost be equal.
+                // this is not important for functionality but for testing purposes
+                if (candidateCost == cost && optPlane.axisCoordinate < candidatePlane.axisCoordinate) {
+                    return;
+                }
+                if (candidateCost <= cost) {
+                    cost = candidateCost;
+                    optPlane = candidatePlane;
+                    callbackArgs = std::tuple<std::decay_t<CallbackArgs>...>(std::forward<CallbackArgs>(args)...);
+                }
+            }
+
+            [[nodiscard]] Plane getOptimalPlane() const {
+                return optPlane;
+            }
+
+            [[nodiscard]] double getCost() const {
+                return cost;
+            }
+
+            std::variant<TriangleIndexVectors<2>, PlaneEventVectors<2>> getPointsSplit() {
+                return std::apply(boundGeometrySplit, std::move(callbackArgs));
+            }
         };
 
 
