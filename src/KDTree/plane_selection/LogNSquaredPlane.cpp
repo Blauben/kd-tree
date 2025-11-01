4b84e75ab313a329ed437ec41c37ea261bd3f37c
Ben Frauenknecht <ben@amatala.de>
2025-11-01 16:57:43 +0100

    OptimalPlane in progress

--- src/KDTree/plane_selection/LogNSquaredPlane.cpp
+++ src/KDTree/plane_selection/LogNSquaredPlane.cpp
@@ -4,37 +4,25 @@ namespace kdtree {
     // O(N*log^2(N)) implementation
     std::tuple<Plane, double, std::variant<TriangleIndexVectors<2>, PlaneEventVectors<2> > >
     LogNSquaredPlane::findPlane(const SplitParam &splitParam) {
-        Plane optPlane{};
-        double cost{std::numeric_limits<double>::infinity()};
-        PlaneEventVector optimalEvents{};
-        bool minSide{true};
+        const std::function geoSubsetCallback = [](const Plane& optPlane, const PlaneEventVector& events, const bool minSideChosen) {
+            return generateTriangleSubsets(events, optPlane, minSideChosen);
+        };
+        OptimalPlane<PlaneEventVector, bool> optPlane{splitParam, geoSubsetCallback};
         for (const auto dimension: ALL_DIRECTIONS) {
             splitParam.splitDirection = dimension;
-            auto [candidatePlane, candidateCost, events, minSideChosen] = findPlaneForSingleDimension(splitParam);
+            findPlaneForSingleDimension(optPlane);
             // this if clause exists to consistently build the same KDTree (choose plane with lower coordinate) by eliminating indeterministic behavior should the cost be equal.
             // this is not important for functionality but for testing purposes
-            if (candidateCost == cost && optPlane.axisCoordinate < candidatePlane.axisCoordinate) {
-                continue;
-            }
-            if (candidateCost < cost) {
-                optPlane = candidatePlane;
-                cost = candidateCost;
-                optimalEvents = events;
-                minSide = minSideChosen;
-            }
         }
         //generate the triangle index lists for the child bounding boxes and return them along with the optimal plane and the plane's cost.
-        return {optPlane, cost, generateTriangleSubsets(optimalEvents, optPlane, minSide)};
+        return {optPlane.getOptimalPlane(), optPlane.getCost(), optPlane.getPointsSplit()};
     }
 
-    std::tuple<Plane, double, PlaneEventVector, bool> LogNSquaredPlane::findPlaneForSingleDimension(
-        const SplitParam &splitParam) {
-        const PlaneEventVector events{std::move(generatePlaneEventsFromFaces(splitParam, {splitParam.splitDirection}))};
-        TriangleCounter triangleCounter{1, {0, countFaces(splitParam.boundFaces), 0}};
-        auto [optPlane, cost, minSide] = traversePlaneEvents(events, triangleCounter, splitParam.boundingBox);
-        return {optPlane, cost, events, minSide};
+    void LogNSquaredPlane::findPlaneForSingleDimension(OptimalPlane<PlaneEventVector, bool>& optPlane) {
+        const PlaneEventVector events{std::move(generatePlaneEventsFromFaces(optPlane.splitParam, {optPlane.splitParam.splitDirection}))};
+        TriangleCounter triangleCounter{1, {0, countFaces(optPlane.splitParam.boundFaces), 0}};
+        traversePlaneEvents(optPlane, events, triangleCounter, optPlane.splitParam.boundingBox);
     }
-
 
     TriangleIndexVectors<2> LogNSquaredPlane::generateTriangleSubsets(const PlaneEventVector &planeEvents,
                                                                       const Plane &plane, const bool minSide) {
--- src/KDTree/plane_selection/PlaneEventAlgorithm.h
+++ src/KDTree/plane_selection/PlaneEventAlgorithm.h
@@ -99,8 +99,53 @@ struct SplitParam;
          * @param boundingBox The current node's bounding box
          * @return Tuple of optimal plane, its cost and where to include planar faces.
          */
-        static std::tuple<Plane, double, bool> traversePlaneEvents(const PlaneEventVector &events,
+        template <typename... OptimalPlaneArgs>
+        static void traversePlaneEvents(OptimalPlane<OptimalPlaneArgs...>& optPlane, const PlaneEventVector &events,
                                                                    TriangleCounter &triangleCounter,
-                                                                   const Box &boundingBox);
+                                                                   const Box &boundingBox) {
+        //traverse all the events
+        int i{0};
+        while (i < events.size()) {
+            //poll a plane to test
+            const Plane &candidatePlane = events[i].plane;
+            //for each plane calculate the faces whose vertices lie in the plane. Differentiate between the face starting in the plane, ending in the plane or all vertices lying in the plane
+            size_t p_start{0}, p_end{0}, p_planar{0};
+            //count all faces that end in the plane, this works because the PlaneEvents are sorted by position and then by PlaneEventType
+            while (i < events.size() && events[i].plane.orientation == candidatePlane.orientation && events[i].plane.
+                   axisCoordinate == candidatePlane.axisCoordinate && events[i].type == PlaneEventType::ending) {
+                p_end++;
+                i++;
+            }
+            //count all the faces that lie in the plane
+            while (i < events.size() && events[i].plane.orientation == candidatePlane.orientation && events[i].plane.
+                   axisCoordinate == candidatePlane.axisCoordinate && events[i].type == PlaneEventType::planar) {
+                p_planar++;
+                i++;
+            }
+            //count all the faces that start in the plane
+            while (i < events.size() && events[i].plane.orientation == candidatePlane.orientation && events[i].plane.
+                   axisCoordinate == candidatePlane.axisCoordinate && events[i].type == PlaneEventType::starting) {
+                p_start++;
+                i++;
+            }
+            //reference to the triangle counter of the current dimension -> better readability
+            triangleCounter.setPlanar(candidatePlane.orientation, p_planar);
+            triangleCounter.updateMax(candidatePlane.orientation, p_planar, p_end);
+            //evaluate plane and update should the new plane be more efficient
+            auto [candidateCost, minSideChosen] = costForPlane(boundingBox, candidatePlane,
+                                                               triangleCounter.getMin(candidatePlane.orientation),
+                                                               triangleCounter.getMax(candidatePlane.orientation),
+                                                               triangleCounter.getPlanar(candidatePlane.orientation));
+            // this condition exists to consistently build the same KDTree (choose plane with lower coordinate) by eliminating indeterministic behavior should the cost be equal.
+            // this is not important for functionality but for testing purposes
+            bool skipEvaluation = candidateCost == optPlane.getCost() && optPlane.getOptimalPlane().axisCoordinate < candidatePlane.axisCoordinate;
+            if (!skipEvaluation) {
+                optPlane.evaluatePlane(candidatePlane, candidateCost, minSideChosen);
+            }
+            //shift the plane to the next candidate and prepare next iteration
+            triangleCounter.updateMin(candidatePlane.orientation, p_planar, p_start);
+            triangleCounter.setPlanar(candidatePlane.orientation, 0);
+        }
+    }
     };
 } // namespace kdtree
