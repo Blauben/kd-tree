2d174434c52ae026111b27a5682853d93dd60af8
Ben Frauenknecht <frb@in.tum.de>
2025-11-17 09:28:01 +0100

    Always fetch gtest to ensure gmock is present

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -3,20 +3,24 @@ include(FetchContent)
 message(STATUS "Setting up Google Test")
 set(GOOGLE_TEST_VERSION 1.15.2)
 
-find_package(GTest ${GOOGLE_TEST_VERSION} QUIET)
+# Don't search for system GTest at all - always fetch
+set(BUILD_GMOCK ON CACHE BOOL "" FORCE)
+set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
 
-if (${GTest_FOUND})
-    message(STATUS "Found existing Google Test: ${GTest_DIR}")
-else ()
-    message(STATUS "Using Google Test from GitHub Release ${GOOGLE_TEST_VERSION}")
+message(STATUS "Fetching Google Test from GitHub Release ${GOOGLE_TEST_VERSION}")
 
-    FetchContent_Declare(googletest
-            GIT_REPOSITORY https://github.com/google/googletest.git
-            GIT_TAG v${GOOGLE_TEST_VERSION}
-    )
-    FetchContent_MakeAvailable(googletest)
+FetchContent_Declare(googletest
+    GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_TAG v${GOOGLE_TEST_VERSION}
+)
+FetchContent_MakeAvailable(googletest)
 
-    target_compile_options(gtest_main PRIVATE -w)
-    get_target_property(propval gtest_main INTERFACE_INCLUDE_DIRECTORIES)
-    target_include_directories(gtest_main SYSTEM PUBLIC "${propval}")
-endif ()
+# Suppress warnings from gtest
+target_compile_options(gtest PRIVATE -w)
+target_compile_options(gtest_main PRIVATE -w)
+target_compile_options(gmock PRIVATE -w)
+target_compile_options(gmock_main PRIVATE -w)
+
+# Make include directories system to suppress warnings
+get_target_property(propval gtest_main INTERFACE_INCLUDE_DIRECTORIES)
+target_include_directories(gtest_main SYSTEM PUBLIC "${propval}")
