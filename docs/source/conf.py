e1e7386ec894b0798c4083710577a2ed57ee1032
Ben Frauenknecht <ben.frauenknecht@tum.de>
2025-11-24 02:17:58 +0100

    Setup unstructered documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,42 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'KDTree'
+copyright = '2025, Ben Frauenknecht'
+author = 'Ben Frauenknecht'
+release = 'v1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx_rtd_theme', 'breathe']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Breathe Configuration
+
+xml_dir_file = os.path.join('..', 'doxygen_out_dir.txt')
+with open(xml_dir_file) as f:
+    xml_dir = os.path.join(f.read().strip(), 'xml')
+
+breathe_projects = {
+    # "KDTree": xml_dir
+    "KDTree": "../../cmake-build-release/docs/doxygen/xml"
+}
+breathe_default_project = "KDTree"
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,14 @@
+.. KDTree documentation master file, created by
+   sphinx-quickstart on Sun Nov 23 23:01:20 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+KDTree documentation
+====================
+
+.. doxygennamespace:: kdtree
+    :project: KDTree
+    :members:
+    :protected-members:
+    :private-members:
+    :undoc-members:
