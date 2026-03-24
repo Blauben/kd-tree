66ddce6a31467be1743a66056a29e5128b37d324
Ben Frauenknecht <ben@amatala.de>
2026-03-24 20:47:44 +0100

    Add prototype leaf node register

--- src/KDTree/tree/LeafNode.cpp
+++ src/KDTree/tree/LeafNode.cpp
@@ -3,8 +3,42 @@
 namespace kdtree {
     LeafNode::LeafNode(const SplitParam &splitParam, const size_t nodeId)
         : TreeNode(splitParam, nodeId) {
+        leafNodes.push_back(this);
         LOG_DEBUG("LeafNode: Constructed with nodeId " + std::to_string(nodeId));
     }
+
+    LeafNode::LeafNode(LeafNode && other) noexcept : TreeNode(std::move(other)) {
+        auto it = std::find(leafNodes.begin(), leafNodes.end(), &other);
+        if (it != leafNodes.end())
+            *it = this;  // replace in-place, preserving order
+    }
+
+    LeafNode& LeafNode::operator=(LeafNode && other) noexcept {
+        if (this != &other) {
+            TreeNode::operator=(std::move(other));
+
+            // Remove 'this' from registry (its slot will be replaced by other's)
+            std::erase_if(leafNodes, [this](const LeafNode* leaf) {
+                return leaf == this;
+            });
+
+            // Redirect 'other' slot to 'this'
+            auto it = std::find(leafNodes.begin(), leafNodes.end(), &other);
+            if (it != leafNodes.end())
+                *it = this;
+        }
+        return *this;
+    }
+
+    LeafNode::~LeafNode() {
+        // No-op if already removed by move operation
+        std::erase_if(leafNodes, [this](const LeafNode* leaf) {
+            return leaf == this;
+        });
+        LOG_DEBUG("LeafNode: Destroyed with nodeId " + std::to_string(nodeId));
+    }
+
+    std::vector<LeafNode*> LeafNode::leafNodes{};
 
     void LeafNode::getIntersections(const Vertex &origin, const Vertex &ray,
                                     std::set<Vertex> &intersections) {
@@ -28,83 +62,83 @@ namespace kdtree {
                 }
                          });
         LOG_DEBUG("LeafNode: getIntersections finished for nodeId " + std::to_string(this->nodeId));
+    }
+
+    std::optional<Vertex> LeafNode::rayIntersectsObject(const Vertex &rayOrigin, const Vertex &rayVector,
+                                                        const GeometryObject &object) {
+        if (object.getIndexVector().size() == 3) {
+            return rayIntersectsTriangle(rayOrigin, rayVector, object.getVertices());
         }
-
-        std::optional<Vertex> LeafNode::rayIntersectsObject(const Vertex &rayOrigin, const Vertex &rayVector,
-                                                            const GeometryObject &object) {
-            if (object.getIndexVector().size() == 3) {
-                return rayIntersectsTriangle(rayOrigin, rayVector, object.getVertices());
-            }
-            if (object.getIndexVector().size() == 1) {
-                return rayIntersectsPoint(rayOrigin, rayVector, object.getVertices()[0]);
-            }
-            LOG_ERROR("LeafNode: rayIntersectsObject called with invalid object (neither triangle nor point)");
-            throw std::runtime_error("Error: rayIntersectsObject called with neither a triangle nor a point.");
+        if (object.getIndexVector().size() == 1) {
+            return rayIntersectsPoint(rayOrigin, rayVector, object.getVertices()[0]);
         }
+        LOG_ERROR("LeafNode: rayIntersectsObject called with invalid object (neither triangle nor point)");
+        throw std::runtime_error("Error: rayIntersectsObject called with neither a triangle nor a point.");
+    }
 
-        std::optional<Vertex> LeafNode::rayIntersectsTriangle(const Vertex &rayOrigin, const Vertex &rayVector,
-                                                              const std::vector<Vertex> &triangleVertices) {
-            // Adapted Möller–Trumbore intersection algorithm
-            // see https://en.wikipedia.org/wiki/Möller–Trumbore_intersection_algorithm
-            using namespace kdtree;
-            using namespace util;
-            const Vertex edge1 = triangleVertices[1] - triangleVertices[0];
-            const Vertex edge2 = triangleVertices[2] - triangleVertices[0];
-            const Vertex h = cross(rayVector, edge2);
-            const double a = dot(edge1, h);
-            if (a > -EPSILON_ZERO_OFFSET && a < EPSILON_ZERO_OFFSET) {
-                return std::nullopt;
-            }
-
-            const double f = 1.0 / a;
-            const Vertex s = rayOrigin - triangleVertices[0];
-            const double u = f * dot(s, h);
-            if (u < 0.0 || u > 1.0) {
-                return std::nullopt;
-            }
-
-            const Vertex q = cross(s, edge1);
-            const double v = f * dot(rayVector, q);
-            if (v < 0.0 || u + v > 1.0) {
-                return std::nullopt;
-            }
-
-            const double t = f * dot(edge2, q);
-            if (t > EPSILON_ZERO_OFFSET) {
-                return rayOrigin + rayVector * t;
-            }
+    std::optional<Vertex> LeafNode::rayIntersectsTriangle(const Vertex &rayOrigin, const Vertex &rayVector,
+                                                          const std::vector<Vertex> &triangleVertices) {
+        // Adapted Möller–Trumbore intersection algorithm
+        // see https://en.wikipedia.org/wiki/Möller–Trumbore_intersection_algorithm
+        using namespace kdtree;
+        using namespace util;
+        const Vertex edge1 = triangleVertices[1] - triangleVertices[0];
+        const Vertex edge2 = triangleVertices[2] - triangleVertices[0];
+        const Vertex h = cross(rayVector, edge2);
+        const double a = dot(edge1, h);
+        if (a > -EPSILON_ZERO_OFFSET && a < EPSILON_ZERO_OFFSET) {
             return std::nullopt;
         }
 
-        std::optional<Vertex> LeafNode::rayIntersectsPoint(const Vertex &rayOrigin, const Vertex &rayVector, const Vertex &center) {
-            using namespace util;
-            constexpr double EPSILON_OFFSET = 1e-2 /* TODO: get radius from object, or use a default small value */;
-
-            // Vector from ray origin to sphere center
-            const Vertex oc = rayOrigin - center;
-
-            // Calculate the nearest intersection point
-            const double t = -dot(rayVector, oc) / dot(rayVector, rayVector);
-
-            const Vertex intersection = rayOrigin + rayVector * t;
-
-            const double distance = dot(intersection - center, intersection - center);
-            if (distance > EPSILON_OFFSET) {
-                return std::nullopt;
-            }
-
-            // Return the intersection point
-            return intersection;
+        const double f = 1.0 / a;
+        const Vertex s = rayOrigin - triangleVertices[0];
+        const double u = f * dot(s, h);
+        if (u < 0.0 || u > 1.0) {
+            return std::nullopt;
         }
 
-        std::string LeafNode::toString() const {
-            std::stringstream sstream{};
-            sstream << "LeafNode ID: " << this->nodeId << ", Depth: " << recursionDepth(this->nodeId) << std::endl;
-            return sstream.str();
+        const Vertex q = cross(s, edge1);
+        const double v = f * dot(rayVector, q);
+        if (v < 0.0 || u + v > 1.0) {
+            return std::nullopt;
         }
 
-        std::ostream &operator<<(std::ostream &os, const LeafNode &node) {
-            os << node.toString();
-            return os;
+        const double t = f * dot(edge2, q);
+        if (t > EPSILON_ZERO_OFFSET) {
+            return rayOrigin + rayVector * t;
         }
-    }// namespace kdtree
+        return std::nullopt;
+    }
+
+    std::optional<Vertex> LeafNode::rayIntersectsPoint(const Vertex &rayOrigin, const Vertex &rayVector, const Vertex &center) {
+        using namespace util;
+        constexpr double EPSILON_OFFSET = 1e-2 /* TODO: get radius from object, or use a default small value */;
+
+        // Vector from ray origin to sphere center
+        const Vertex oc = rayOrigin - center;
+
+        // Calculate the nearest intersection point
+        const double t = -dot(rayVector, oc) / dot(rayVector, rayVector);
+
+        const Vertex intersection = rayOrigin + rayVector * t;
+
+        const double distance = dot(intersection - center, intersection - center);
+        if (distance > EPSILON_OFFSET) {
+            return std::nullopt;
+        }
+
+        // Return the intersection point
+        return intersection;
+    }
+
+    std::string LeafNode::toString() const {
+        std::stringstream sstream{};
+        sstream << "LeafNode ID: " << this->nodeId << ", Depth: " << recursionDepth(this->nodeId) << std::endl;
+        return sstream.str();
+    }
+
+    std::ostream &operator<<(std::ostream &os, const LeafNode &node) {
+        os << node.toString();
+        return os;
+    }
+}// namespace kdtree
