94e7693d727576165482e7a0b671d7a24d718b0c
Ben Frauenknecht <ben@amatala.de>
2026-03-25 10:49:46 +0100

    Use weak_ptr

--- src/KDTree/tree/TreeNodeFactory.h
+++ src/KDTree/tree/TreeNodeFactory.h
@@ -28,7 +28,7 @@ namespace kdtree::TreeNodeFactory {
         * @param splitParam Parameters for intersection testing and child node creation. {@link SplitParam}
         * @param nodeId The unique id to be assigned to the newly created node. Follows the convention that the left child gets the id 2 * <current_id> + 1 and
         * the right child 2 * <currrent_id> + 2.
-        * @return A unique pointer to the new TreeNode.
+        * @return A shared pointer to the new TreeNode.
          */
-    std::unique_ptr<TreeNode> createTreeNode(const SplitParam &splitParam, size_t nodeId);
+    std::shared_ptr<TreeNode> createTreeNode(const SplitParam &splitParam, size_t nodeId);
 }// namespace kdtree::TreeNodeFactory
