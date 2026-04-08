1acc169d33157937c472e7d7e174653755c4f43b
Ben Frauenknecht <ben.frauenknecht@tum.de>
2026-04-08 10:07:00 +0200

    Automatic Rebuild LeafNode skeleton

--- src/KDTree/tree/LeafNode.h
+++ src/KDTree/tree/LeafNode.h
@@ -52,6 +52,11 @@ namespace kdtree {
         bool needTreeRebuild();
 
         /**
+         * Only used with dynamic vertices. Checks if the vertices contained in this node have moved outside the node's bounding box since the last tree build, which would require a tree rebuild to maintain correct intersection results.
+         */
+        bool needTreeRebuild();
+
+        /**
         * Used to calculated intersections of a ray and the polyhedron's shapes contained in this node.
         * @param origin The point where the ray originates from.
         * @param ray Specifies the ray direction.
