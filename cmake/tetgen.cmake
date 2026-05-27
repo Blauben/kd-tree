0233014db2a6ea10d9bf7068506224a8385e0de3
Blauben <72825696+Blauben@users.noreply.github.com>
2026-05-27 16:48:52 +0200

    Bundle modified tetgen lib as zip with the project

--- cmake/tetgen.cmake
+++ cmake/tetgen.cmake
@@ -2,42 +2,24 @@ include(FetchContent)
 
 if(NOT TARGET tetgen_lib) 
         message(STATUS "Setting up tetgen")
+        # taken from: https://github.com/libigl/tetgen
 
         set(CMAKE_POLICY_VERSION_MINIMUM 3.10)
+        set(TETGEN_SRC_DIR ${KD_TREE_BINARY_DIR}/_deps/tetgen-src)
+        set(TETGEN_BUILD_DIR ${KD_TREE_BINARY_DIR}/_deps/tetgen-build)
 
-        # IMPORTANT NOTE
-        # We do not use find_package here, as we modify the one source file slightly to suppress output via stdout!!!
-
-        # Fetches the version 1.6 for tetgen
-        FetchContent_Declare(tetgen
-                GIT_REPOSITORY https://github.com/libigl/tetgen.git
-                GIT_TAG 4f3bfba3997f20aa1f96cfaff604313a8c2c85b6 # release 1.6
+        file(ARCHIVE_EXTRACT
+                INPUT ${KD_TREE_SOURCE_DIR}/lib/tetgen_lib.zip
+                DESTINATION ${TETGEN_SRC_DIR}
         )
 
-        FetchContent_MakeAvailable(tetgen)
-
-        # Modify the tetgen library to suppress console output from the printf function
-        if(NOT EXISTS ${tetgen_SOURCE_DIR}/tetgen_mod.cxx)
-        message(STATUS "Creating modified tetgen.cxx in order to prevent console output from library")
-        file(READ ${tetgen_SOURCE_DIR}/tetgen.cxx TETGEN_CXX)
-
-        string(REPLACE
-                "#include \"tetgen.h\""
-                "#include \"tetgen.h\"\n#define printf(fmt, ...) (0)\n"
-                TETGEN_CXX "${TETGEN_CXX}")
-
-        file(WRITE ${tetgen_SOURCE_DIR}/tetgen_mod.cxx
-                "${TETGEN_CXX}"
-        )
-        else()
-        message(STATUS "A modified tetgen.cxx already exists! It is assumed that it is the correct one disabling output")
-        endif()
+        add_subdirectory(${TETGEN_SRC_DIR} ${TETGEN_BUILD_DIR} EXCLUDE_FROM_ALL)
 
         mark_as_advanced(FORCE BUILD_EXECUTABLE BUILD_LIBRARY)
 
         # Add the modified version of the tetgen library
         add_library(tetgen_lib STATIC
-                ${tetgen_SOURCE_DIR}/tetgen_mod.cxx
+                ${tetgen_SOURCE_DIR}/tetgen.cxx
                 ${tetgen_SOURCE_DIR}/predicates.cxx
         )
 
