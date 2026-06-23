adb134695181f7ee3afaf8bdef4df3d94a6d8c43
Blauben <72825696+Blauben@users.noreply.github.com>
2026-06-23 18:35:35 +0200

    Review changes applies

--- cmake/tetgen.cmake
+++ cmake/tetgen.cmake
@@ -8,40 +8,26 @@ if(NOT TARGET tetgen_lib)
         set(TETGEN_SRC_DIR ${KD_TREE_BINARY_DIR}/_deps/tetgen-src)
         set(TETGEN_BUILD_DIR ${KD_TREE_BINARY_DIR}/_deps/tetgen-build)
 
-        file(ARCHIVE_EXTRACT
-                INPUT ${KD_TREE_SOURCE_DIR}/lib/tetgen_lib.zip
-                DESTINATION ${TETGEN_SRC_DIR}
+        FetchContent_Declare(tetgen
+                URL ${KD_TREE_SOURCE_DIR}/lib/tetgen_lib.zip
+                URL_HASH SHA256=795c17c869b6e7cccabf31ac775304a7837c828a14d06173b25c5276ea65fbf8
+                DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+       )
+
+       FetchContent_MakeAvailable(tetgen)
+
+        # Include the tetgen source directory for the library. Use BUILD/INSTALL
+        # interface generator expressions to avoid exporting absolute paths.
+        target_include_directories(tetgen INTERFACE
+                $<BUILD_INTERFACE:${tetgen_SOURCE_DIR}>
+                $<INSTALL_INTERFACE:include/tetgen>
         )
-
-        add_subdirectory(${TETGEN_SRC_DIR} ${TETGEN_BUILD_DIR} EXCLUDE_FROM_ALL)
-
-        mark_as_advanced(FORCE BUILD_EXECUTABLE BUILD_LIBRARY)
-
-        # Add the modified version of the tetgen library
-        add_library(tetgen_lib STATIC
-                ${tetgen_SOURCE_DIR}/tetgen.cxx
-                ${tetgen_SOURCE_DIR}/predicates.cxx
-        )
-
-        # Define the TETLIBRARY macro for usage
-        target_compile_definitions(tetgen_lib PRIVATE -DTETLIBRARY)
-
-                # Include the tetgen source directory for the library. Use BUILD/INSTALL
-                # interface generator expressions to avoid exporting absolute paths.
-                target_include_directories(tetgen_lib INTERFACE
-                        $<BUILD_INTERFACE:${tetgen_SOURCE_DIR}>
-                        $<INSTALL_INTERFACE:include/tetgen>
-                )
-
-        # Disable warnings from the library target
-        target_compile_options(tetgen_lib PRIVATE -w)
-
 else()
         message(STATUS "tetgen library already exists in the project. Using existing target. CAUTION: library modifications may not be applied.")
 endif()
 
 if(NOT TARGET tetgen::tetgen)
-        add_library(tetgen::tetgen ALIAS tetgen_lib)
+        add_library(tetgen::tetgen ALIAS tetgen)
 else()
         message(STATUS "tetgen::tetgen target already exists. Using existing target.")
 endif()
